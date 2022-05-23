9ec00ab5bf842d9647a47630aeabcbf8af0fda4e
Abner Persio <binhopersio@gmail.com>
2022-05-23 20:36:10 -0300

    feat: ud button component

---
+++ src/components/Button/index.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import Spinner from '../Spinner';
+import { StyledButton } from './styles';
+
+export default function Button({ type, disabled, isLoading, children }) {
+  return (
+    <StyledButton type={type} disabled={disabled || isLoading}>
+      {!isLoading && children}
+      {isLoading && <Spinner size={16} />}
+    </StyledButton>
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.string,
+  isLoading: PropTypes.bool,
+  disabled: PropTypes.bool,
+  children: PropTypes.node.isRequired,
+};
+
+Button.defaultProps = {
+  type: 'button',
+  disabled: false,
+  isLoading: false,
+};
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components';
 
-export default styled.button`
+export const StyledButton = styled.button`
   border: none;
   border-radius: 4px;
   font-weight: bold;
@@ -11,6 +11,9 @@ export default styled.button`
   color: white;
   box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
   transition: background-color 0.2s ease-in;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.primary.light};
