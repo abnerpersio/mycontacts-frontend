3603adef9b448ec434d19693a57fe025b0c33622
Abner Persio <binhopersio@gmail.com>
2022-10-31 12:18:07 -0300

    refactor: use is mounted hook

--- src/pages/EditContact/index.js
+++ src/pages/EditContact/index.js
@@ -6,10 +6,13 @@ import ContactsService from '../../services/ContactsService';
 import Loader from '../../components/Loader';
 import { toast } from '../../utils/toast';
 
+import useIsMounted from '../../hooks/useIsMounted';
+
 export default function EditContact() {
   const [isLoading, setIsLoading] = useState(true);
   const [contactName, setContactName] = useState('');
   const contactFormRef = useRef(null);
+  const isMounted = useIsMounted();
 
   const { id } = useParams();
   const history = useHistory();
@@ -18,17 +21,22 @@ export default function EditContact() {
     async function loadContact() {
       try {
         const contact = await ContactsService.getContact(id);
+
+        if (!isMounted()) return;
+
         contactFormRef.current.setFields(contact);
         setContactName(contact.name);
         setIsLoading(false);
-      } catch (error) {
+      } catch {
+        if (!isMounted()) return;
+
         history.push('/');
         toast({ type: 'danger', text: 'Contato não encontrado!' });
       }
     }
 
     loadContact();
-  }, [id, history]);
+  }, [id, history, isMounted]);
 
   async function handleSumit(params) {
     try {
