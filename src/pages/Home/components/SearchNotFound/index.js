9694ad9c034cb4b57ffc21a1f929d05934a4307b
Abner Persio <binhopersio@gmail.com>
2024-03-18 16:06:39 -0300

    refactor: home page components

---
+++ src/pages/Home/components/SearchNotFound/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import PropTypes from 'prop-types';
+import { Container } from './styles';
+import magnifierQuestion from '../../../../assets/images/icons/magnifier-question.svg';
+
+export default function SearchNotFound(props) {
+  const { searchTerm } = props;
+
+  return (
+    <Container>
+      <img src={magnifierQuestion} alt="Magnifier question" />
+
+      <span>
+        Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>.
+      </span>
+    </Container>
+  );
+}
+
+SearchNotFound.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+};
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,13 @@
-/* eslint-disable react/jsx-one-expression-per-line */
-import { Link } from 'react-router-dom';
-import { Container, ListHeader, Card, EmptyListContainer, SearchNotFoundContainer } from './styles';
-import arrow from '../../assets/images/icons/arrow.svg';
-import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
-import emptyBox from '../../assets/images/icons/empty-box.svg';
-import magnifierQuestion from '../../assets/images/icons/magnifier-question.svg';
+import { Container } from './styles';
 import Loader from '../../components/Loader';
-import Modal from '../../components/Modal';
 import { useHome } from './useHome';
 import InputSearch from './components/InputSerach';
 import Header from './components/Header';
 import ErrorStatus from './components/ErrorStatus';
+import EmptyList from './components/EmptyList';
+import SearchNotFound from './components/SearchNotFound';
+import ContactsList from './components/ContactsList';
+import Modal from '../../components/Modal';
 
 export default function Home() {
   const {
@@ -48,58 +44,23 @@ export default function Home() {
 
       {!hasError && (
         <>
-          {contacts.length < 1 && !isLoading && (
-            <EmptyListContainer>
-              <img src={emptyBox} alt="Empty box" />
-
-              <p>
-                Você ainda não tem nenhum contato cadastrado! Clique no botão
-                <strong> ”Novo contato” </strong>à cima para cadastrar o seu primeiro!
-              </p>
-            </EmptyListContainer>
-          )}
+          {contacts.length < 1 && !isLoading && <EmptyList />}
 
           {contacts.length > 0 && filteredContacts.length < 1 && (
-            <SearchNotFoundContainer>
-              <img src={magnifierQuestion} alt="Magnifier question" />
-
-              <span>
-                Nenhum resultado foi encontrado para <strong>{searchTerm}</strong>.
-              </span>
-            </SearchNotFoundContainer>
+            <SearchNotFound searchTerm={searchTerm} />
           )}
 
-          {filteredContacts.length > 0 && (
-            <ListHeader orderBy={orderBy}>
-              <button onClick={handleToggleOrderBy} type="button">
-                <span>Nome</span>
-                <img src={arrow} alt="Arrow" />
-              </button>
-            </ListHeader>
-          )}
-
-          {filteredContacts.map((contact) => (
-            <Card key={contact.id}>
-              <div className="info">
-                <div className="contact-name">
-                  <strong>{contact.name}</strong>
-                  {contact.category.name && <small>{contact.category.name}</small>}
-                </div>
-                <span>{contact.email}</span>
-                <span>{contact.phone}</span>
-              </div>
-
-              <div className="actions">
-                <Link to={`/edit/${contact.id}`}>
-                  <img src={edit} alt="Edit" />
-                </Link>
-
-                <button type="button" onClick={() => handleDeleteContact(contact)}>
-                  <img src={trash} alt="Delete" />
-                </button>
-              </div>
-            </Card>
-          ))}
+          <ContactsList
+            filteredContacts={filteredContacts}
+            isDeleteModalVisible={isDeleteModalVisible}
+            isLoadingDelete={isLoadingDelete}
+            contactBeingDeleted={contactBeingDeleted}
+            orderBy={orderBy}
+            onToggleOrderBy={handleToggleOrderBy}
+            onCloseDeleteModal={handleCloseDeleteModal}
+            onConfirmDeleteContact={handleConfirmDeleteContact}
+            onDeleteContact={handleDeleteContact}
+          />
         </>
       )}
 
