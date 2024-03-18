1d925c55ee8642d147dcb24b9629bb1191f57d38
Abner Persio <binhopersio@gmail.com>
2024-03-18 17:28:55 -0300

    chore: remove set state from close modal

--- src/pages/Home/useHome.js
+++ src/pages/Home/useHome.js
@@ -67,7 +67,6 @@ export function useHome() {
 
   function handleCloseDeleteModal() {
     setIsDeleteModalVisible(false);
-    setContactBeingDeleted(null);
   }
 
   async function handleConfirmDeleteContact() {
